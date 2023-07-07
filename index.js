96d1fd3f4c6d9afe00d0a341af244f91461aa429
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-07 21:11:14 +0300

    finish lecture 4

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/text.txt`, (err, data) => {
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    if (err) console.log(err.message);
-
-    console.log(res.body.message);
-    fs.writeFile('dog-image.txt', res.body.message, (err) => {
-      if (err) console.log(err.message);
-
-      console.log('Random image saved');
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file');
+      resolve(data);
     });
   });
-});
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write file');
+      resolve('Success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/text.txt`)
+  .then(data => {
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-image.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
