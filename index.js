9a8af43ec4be69f663f436101f7df87095776ab4
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-08 14:17:21 +0300

    finish lecture 5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 const readFilePro = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject('I could not find that file');
+      if (err) reject('I could not find the file');
       resolve(data);
     });
   });
@@ -19,17 +19,34 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/text.txt`)
-  .then(data => {
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then(res => {
+// readFilePro(`${__dirname}/text.txt`)
+//   .then(data => {
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-image.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file!');
+//   })
+//   .catch(err => {
+//     console.log(err.message);
+//   });
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/text.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
     console.log(res.body.message);
-    return writeFilePro('dog-image.txt', res.body.message);
-  })
-  .then(() => {
+
+    await writeFilePro('dog-image.txt', res.body.message);
     console.log('Random dog image saved to file!');
-  })
-  .catch(err => {
-    console.log(err.message);
-  });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
